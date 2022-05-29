d178a49cc5b16963f3fe1695aef1c1d90c37fa4a
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-29 17:36:16 +0700

    add verify user in login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { infoUser, login, register } from '../controllers/auth.js';
+import { verifyInfoUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:email', infoUser);
+router.get('/:email', verifyInfoUser, infoUser);
 router.post('/register', register)
 router.post('/login', login)
 
