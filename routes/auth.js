476b88c208f0bfd14028fe200fb85c42cc47a89e
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-29 17:39:46 +0700

    delete auth in info

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { infoUser, login, register } from '../controllers/auth.js';
-import { verifyInfoUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:email', verifyInfoUser, infoUser);
+router.get('/:email', infoUser);
 router.post('/register', register)
 router.post('/login', login)
 
