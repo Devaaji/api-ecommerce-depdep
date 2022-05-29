347cd252c0a3f26df2d3874cd8353134abb3714c
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-29 17:45:59 +0700

    verify token in login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { infoUser, login, register } from '../controllers/auth.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:email', infoUser);
+router.get('/:email', verifyToken, infoUser);
 router.post('/register', register)
 router.post('/login', login)
 
